5335667bbaeb642090779e88cf2350f567704154
ConfiG <cgytrus@cgyt.ru>
2025-08-18 04:54:33 +0300

    load early this might break things idk

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import { bootstrapApp } from "./app";
 
 (function () {
   "use strict";
-  window.onload = () => {
-    bootstrapApp().catch((e) =>
-      console.error("Overlay Pro bootstrap failed", e),
-    );
-  };
+  bootstrapApp().catch((e) =>
+    console.error("Overlay Pro bootstrap failed", e),
+  );
 })();
 export {};
 
